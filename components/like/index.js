6e6c011ad802ffa65194ea3f91d0931999fa5026
yuec621 <yuec621@outlook.com>
2019-03-14 16:59:03 +0800

    自定义组件

--- components/like/index.js
+++ components/like/index.js
@@ -1,12 +1,48 @@
-// pages/index/index.js
-Page({
+// components/like/index.js
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties:{
+    /*属性也可被数据绑定 */
+    like:{
+      type:Boolean,
+
+    },
+    count:{
+      type:Number
+    }
+  },
 
   /**
    * 页面的初始数据
    */
   data: {
-
+      //数据绑定
+      //三元表达式
+      //组件封装，开放，粒度
+      
+      yesSrc:'images/like.png',
+      noSrc:'images/like@dis.png'
   },
+
+  /**
+   * 组件方法列表
+   */
+  methods:{
+    onLike:function(event){
+      let like=this.properties.like
+      let count=this.properties.count
+      count=like?count-1:count+1
+      /*数据更新 */
+      this.setData({
+        count:count,
+        like:!like
+      })
+    }
+  },
+
 
   /**
    * 生命周期函数--监听页面加载
