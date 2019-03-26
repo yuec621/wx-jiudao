958bac6141cc732f9fe8fb5924cca0f5df91a0e4
yuec621 <yuec621@outlook.com>
2019-03-26 16:02:20 +0800

    音频

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -1,18 +1,26 @@
 // components/classic/music/index.js
 import {classicBeh} from '../classic-beh.js'
+const mMgr=wx.getBackgroundAudioManager()
+
 Component({
   /**
    * 组件的属性列表
    */
   behaviors:[classicBeh],
   properties: {
-    
+    src:String,
+    title:String
+   
   },
 
   /**
    * 组件的初始数据
    */
+  /**
+   * 播放音乐
+   */
   data: {
+    playing:false,
     pauseSrc:'images/player@pause.png',
     playSrc:'images/player@play.png'
   },
@@ -21,6 +29,24 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    onPlay:function(event){
+      
+      //图片切换
+      if(!this.data.playing){
+        
+        this.setData({
+          playing:true
+        })
+       mMgr.title = this.properties.title
+       mMgr.src=this.properties.src
+      }else{
+        this.setData({
+          playing:false
+        })
+         
+      mMgr.pause()
+      }
+      
+    }
   }
 })
