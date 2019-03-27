a8e8998cc6f3c28f92a72e1b543e66c95e01d3f4
yuec621 <yuec621@outlook.com>
2019-03-27 15:59:12 +0800

    promise重构

--- pages/book/book.js
+++ pages/book/book.js
@@ -1,4 +1,8 @@
 // pages/book/book.js
+import {
+  BookModel
+} from '../../models/book.js'
+const bookModel=new BookModel()
 Page({
 
   /**
@@ -12,7 +16,41 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    bookModel.getHotList().then(res=>{
+      console.log(res)
+    })
+    /*
+  const hotList= bookModel.getHotList()
+  hotList.then(
+    res=>{
+      console.log(res)
+    const count=  bookModel.getMyBookCount()
+    .then(res=>{
+      console.log(res)
+    })
+    }
+    )
+  */
 
+    //Promise对象
+    //promise三种状态：pending--进行中 fulfilled--已成功 rejected--已失败
+    /*
+    const promise=new Promise((resolve,reject)=>{
+      wx.getSystemInfo({
+        success:(res)=>{
+          resolve(res)
+        },
+        fail:(error)=>{
+          reject(error)
+        }
+      })
+    })
+    promise.then((res)=>{
+      console.log(res)
+    },(error)=>{
+      console.log(error)
+    })
+    */
   },
 
   /**
