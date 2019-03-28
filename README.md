50066c88667df7efe1cc15b1d41e748126679852
yuec621 <yuec621@outlook.com>
2019-03-28 16:03:26 +0800

    书籍详情页--标签---wxs

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 401找不到api  大小写
 音乐无法正常播放----需加title
+appKey:'an2GxVWfNvRloQhd'
--- components/book/index.js
+++ components/book/index.js
@@ -4,7 +4,11 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    book:Object
+    book:Object,
+    showLike:{
+      type:Boolean,
+      value:true
+    }
   },
 
   /**
@@ -19,10 +23,15 @@ Component({
    */
   methods: {
     onTap(event){
-      const bid=this.properties.book.id 
+      const bid = this.properties.book.id
       wx.navigateTo({
-        url:'/pages/book-detail/book-detail?bid=${bid}'
+        url:`/pages/book-detail/book-detail?bid=${bid}`
       })
+      // 降低了组件的通用性
+      // 非常方便
+      // 服务于当前的项目 项目组件
+      // 
     }
   }
 })
+
